6fde4645490e26eb8c3d3e77549ebc46c8b18d04
Maritsu <maritsuqnn@gmail.com>
2022-08-10 13:35:46 +0200

    ricing slock

--- slock/config.h
+++ slock/config.h
@@ -3,9 +3,9 @@ static const char *user  = "nobody";
 static const char *group = "nobody";
 
 static const char *colorname[NUMCOLS] = {
- 	[BACKGROUND] =   "#222222",     /* after initialization */
-	[INIT] =   "black",     /* after initialization */
-	[INPUT] =  "#227799",   /* during input */
+	[BACKGROUND] = "white",
+ 	[INIT] =   "#242a32",     /* after initialization */
+	[INPUT] =  "#845AE3",   /* during input */
 	[FAILED] = "#772222",   /* wrong password */
 };
 
@@ -15,23 +15,30 @@ static const int failonclear = 1;
 /* allow control key to trigger fail on clear */
 static const int controlkeyclear = 0;
 
-/* insert grid pattern with scale 1:1, the size can be changed with logosize */
 static const int logosize = 75;
-static const int logow = 12;	/* grid width and height for right center alignment*/
+static const int logow = 12; /* Grid width and height for right center alignment*/
 static const int logoh = 6;
 
 static XRectangle rectangles[9] = {
-	/* x	y	w	h */
-	{ 0,	3,	1,	3 },
-	{ 1,	3,	2,	1 },
-	{ 0,	5,	8,	1 },
-	{ 3,	0,	1,	5 },
-	{ 5,	3,	1,	2 },
-	{ 7,	3,	1,	2 },
-	{ 8,	3,	4,	1 },
-	{ 9,	4,	1,	2 },
-	{ 11,	4,	1,	2 },
+    {0, 3, 1, 3},
+    {1, 3, 2, 1},
+    {0, 5, 8, 1},
+    {3, 0, 1, 5},
+    {5, 3, 1, 2},
+    {7, 3, 1, 2},
+    {8, 3, 4, 1},
+    {9, 4, 1, 2},
+    {11,4, 1, 2},
 };
+
+/* Enable blur */
+#define BLUR
+/* Set blur radius */
+static const int blurRadius=32;
+/* Enable Pixelation */
+#define PIXELATION
+/* Set pixelation radius */
+static const int pixelSize=4;
 
 /* time in seconds before the monitor shuts down */
 static const int monitortime = 5;
