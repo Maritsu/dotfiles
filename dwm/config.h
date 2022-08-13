2e6eb41d7c67c0b8b8149b506b9435e7a943392a
Maritsu <maritsuqnn@gmail.com>
2022-08-13 11:48:42 +0200

    dwm, dmenu / moved dmenu to always appear on monitor 0

--- dwm/config.h
+++ dwm/config.h
@@ -54,9 +54,9 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
+	{ "ﱖ",      tile },    /* first entry is default */
+	{ "缾",     NULL },    /* no layout function means floating behavior */
+	{ "",      monocle },
 };
 
 /* key definitions */
@@ -72,7 +72,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont , "-x", "5", "-y", "5", "-z", "512" };
+static const char *dmenucmd[] = { "dmenu_run", "-m", "0", "-fn", dmenufont , "-x", "5", "-y", "5", "-z", "512" }; //FORCE MONITOR 0
 static const char *termcmd[]  = { "st", NULL };
 static const char *lockcmd[]  = { "slock", NULL };
 static const char *killcmd[]  = { "pkill", "dwm", NULL };
