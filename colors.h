5efe1aac05086af257d6ca51741453ed071caca8
Maritsu <maritsuqnn@gmail.com>
2022-09-25 18:06:39 +0200

    added secondary font (japanese)

--- colors.h
+++ colors.h
@@ -2,5 +2,6 @@ static const char GC_bg[] = "#1e1e2e";
 static const char GC_ac[] = "#209fb5";
 static const char GC_hi[] = "#f9e2af";
 static const char GC_fg[] = "#cdd6f4";
-static const char GC_fn[] = "Hack Nerd Font:size=10";
+static char GC_fn[] = "Hack Nerd Font:size=10";
+static char GC_f2[] = "IPAGothic:size=10";
 static const char GC_tf[] = "Hack Nerd Font:size=9";
