cc80d43e33c5d020beaad7ba01f3070837a8a78c
Maritsu <maritsuqnn@gmail.com>
2022-12-27 08:55:49 +0100

    minor addition

--- player.sh
+++ player.sh
@@ -2,6 +2,9 @@
 
 playerctl $1
 [[ "$1" == "stop" ]] && exit
+
+sleep 0.1
 title=$(playerctl metadata --format '{{artist}} - {{title}}')
 text=$(playerctl metadata --format '{{status}}')
+[[ $? -eq 1 ]] && exit
 dunstify -a playerctl -r 105 -u normal "$title" "$text"
