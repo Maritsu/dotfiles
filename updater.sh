3796ab6e8ed22b80ab5853a58e9a40de758c65bb
Maritsu <maritsuqnn@gmail.com>
2023-01-02 18:48:27 +0100

    more work upon updater script

--- updater.sh
+++ updater.sh
@@ -1,28 +1,33 @@
 #!/bin/sh
 # REQUIRES ARCHLINUX
 
-aur=-1
-foo=$(type checkupdates+aur)
-fooes=$?
-if [[ $fooes -ne 0 ]]; then
-  case $fooes in
-    1) aur=0;;
-    *) ;;
-  esac
+[[ "$1" == "-a" ]] && AURF=1 || AURF=0
+
+aur=0
+if [[ $AURF -eq 1 ]]; then
+  aav=$(type checkupdates+aur)
+  aave=$?
+  if [[ $aave -eq 0 ]]; then
+    aur=1
+  else
+    echo -e "\e[1;31m==> ERROR: \e[1;32mcheckupdates+aur \e[1;37mnot found. Dropping back to \e[1;32mcheckupdates\e[1;37m...\e[m"
+  fi
+fi
+
+if [[ $aur -eq 0 ]]; then
+  [[ pr_checkupdates -eq 1 ]] && exit 126
   bar=$(type checkupdates)
   bares=$?
   if [[ $bares -ne 0 ]]; then
-    echo -e "\e[1;31mcheckupdates \e[0;31mnot found. Are you using \[1;36mArch Linux?\e[m"
+    echo -e "\e[1;31m==> ERROR: \e[1;32mcheckupdates \e[1;37mnot found. Are you using \[1;36mArch Linux\e[1;37m?\e[m"
     exit 126
   fi
-else
-  aur=1
 fi
 
 echo -e "\e[1;34m:: \e[1;37mChecking for updates... \e[m"
 
 ul=""
-[[ $aur -eq 1 ]] && ul=$(checkupdates+aur $@) || ul=$(checkupdates $@)
+[[ $aur -eq 1 ]] && ul=$(checkupdates+aur) || ul=$(checkupdates)
 exist=$?
 
 if [[ $exist -ne 0 ]]; then
@@ -38,13 +43,14 @@ while IFS= read -r line; do
   newver=$(echo "$line" | cut -d" " -f4)
   echo -e "$packagename \e[1;31m$oldver\e[m \e[0;30m->\e[m \e[1;32m$newver\e[m"
 done <<< "$ul"
+echo -e "\e[0;36mTotal updates: \e[1;36m$(echo $ul | wc -l)"
 
-printf "\n\e[1;34m:: \e[1;37mDo you want to proceed? \e[0;33m[Y/n] \e[m"
+printf "\n\e[1;34m:: \e[1;37mDo you want to proceed? [Y/n] \e[m"
 read choice
 [[ "$choice" == "" ]] && choice="Y"
 case $choice in
   [yY]*) break;;
-  *) echo -e "\e[0;31mExiting...\e[m"; exit;;
+  *) exit;;
 esac
 
 # Use sudo by default for compatibility
